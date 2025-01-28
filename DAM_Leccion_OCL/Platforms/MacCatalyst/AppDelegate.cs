3d8dd7255df0d1dc6f4357e039a0fbf708ac0135
Oscar200301 <21030082@itesa.edu.mx>
2025-01-28 17:49:13 -0600

    version_0: Se creo el proyecto MAUI vacio.

---
+++ DAM_Leccion_OCL/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DAM_Leccion_OCL
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
