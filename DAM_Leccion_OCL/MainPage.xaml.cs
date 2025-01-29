c0675d7c1bd57aafe6f9c59f5ab3653c712dea8c
Oscar200301 <21030082@itesa.edu.mx>
2025-01-28 18:52:36 -0600

    version_02: Instrucciones basicas en boton y caja de texto.

--- DAM_Leccion_OCL/MainPage.xaml.cs
+++ DAM_Leccion_OCL/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_OCL
+﻿using DAM_Leccion_OCL.Model;
+
+namespace DAM_Leccion_OCL
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,17 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar() {
+            PersonaModel personaModel = new PersonaModel();
+            personaModel.Nombre = "Hola hola";
+            txtNombre.Text = personaModel.Nombre;
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema", "Se ha guardado el registro en la bd", "Aceptar");
         }
     }
 
